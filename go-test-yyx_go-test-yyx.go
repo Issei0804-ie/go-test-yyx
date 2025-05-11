50392c70f7283129668b01be792fca738424b87e
maeken2010 <maeken2010@gmail.com>
2025-05-11 16:53:48 +0900

    testsResultを返すようにした

--- go-test-yyx_go-test-yyx.go
+++ go-test-yyx_go-test-yyx.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	goTestYyx := gotestyyx.GoTestYyx{}
-	calledTestNames, results := goTestYyx.Run([]func() bool{
+	calledTestNames, results, testsResult := goTestYyx.Run([]func() bool{
 		gotestyyx.TestAdd,
 		gotestyyx.TestAdd2,
 	})
@@ -18,8 +18,21 @@ func main() {
 		return
 	}
 
+	//called tests の中に[name, results] 入ってて欲しい
+	if (testsResult.calledTest[0].name != "github.com/Issei0804-ie/go-test-yyx/gotestyyx.TestAdd") || (testsResult.calledTest[0].result != true) || (testsResult.calledTest[1].name != "github.com/Issei0804-ie/go-test-yyx/gotestyyx.TestAdd2") || (testsResult.calledTest[1].result != false) {
+		fmt.Printf(
+			"error: %s %b, %s %b",
+			testsResult.calledTest[0].name,
+			testsResult.calledTest[0].result,
+			testsResult.calledTest[1].name,
+			testsResult.calledTest[1].result,
+		)
+	}
+
 	if results[0] == false {
 		fmt.Printf("error: %s %s", results[0], calledTestNames[0])
+		// loggerを入れる
+		// Type Resultが.Runで返ってくる
 		return
 	}
 
